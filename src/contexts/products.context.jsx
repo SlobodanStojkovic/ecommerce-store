4e8045ccab9ab6b9ebec7027bf7b700f3c451b74
Slobodan Stojkovic <sc.slobodanstojkovic@gmail.com>
2022-05-02 17:05:51 +0200

    Add SHOP_DATA into our Firestore backend

--- src/contexts/products.context.jsx
+++ src/contexts/products.context.jsx
@@ -1,15 +1,25 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
-import PRODUCTS from "../shop-data.json";
+import { addCollectionAndDocuments } from "../utils/firebase/firebase.utils";
+
+import SHOP_DATA from "../shop-data";
 
 export const ProductsContext = createContext({
   products: [],
 });
 
 export const ProductsProvider = ({ children }) => {
-  const [products, setProducts] = useState(PRODUCTS);
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    addCollectionAndDocuments("categories", SHOP_DATA);
+  }, []);  //this is how we have imported categories into our Firestore backend
+
   const value = { products };
+
   return (
-    <ProductsContext.Provider value={value}>{children}</ProductsContext.Provider>
+    <ProductsContext.Provider value={value}>
+      {children}
+    </ProductsContext.Provider>
   );
 };
