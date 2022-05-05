59bdaf6f44d2dca0ebceba6568b8b049f0095bf1
Slobodan Stojkovic <sc.slobodanstojkovic@gmail.com>
2022-05-05 15:58:05 +0200

    Add Redux persist library to keep redux state after refresh

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 
+import { PersistGate } from "redux-persist/integration/react";
+
 import App from "./App";
 
-import { store } from "./store/store";
+import { store, persistor } from "./store/store";
 
 import "./index.scss";
 
@@ -13,9 +15,12 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <PersistGate loading={null} persistor={persistor}>
+        {/* instead of null we can put our loading component, null means not to show anything while loading */}
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
