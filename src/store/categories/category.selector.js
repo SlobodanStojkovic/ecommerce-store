a0d4fc45aa8cb68cb2f6aeb990ceed05be6e39de
Slobodan Stojkovic <sc.slobodanstojkovic@gmail.com>
2022-05-05 13:16:05 +0200

    Update Memoize selectCategoryReducer with RESELECT selector library for Redux

--- src/store/categories/category.selector.js
+++ src/store/categories/category.selector.js
@@ -1,8 +1,19 @@
-export const selectCategoriesMap = (state) => {
-  console.log("selector fired");
-  return state.categories.categories.reduce((acc, category) => {
-    const { title, items } = category;
-    acc[title.toLowerCase()] = items;
-    return acc;
-  }, {});
-};
+import { createSelector } from "reselect";
+
+const selectCategoryReducer = (state) => state.categories;
+
+export const selectCategories = createSelector(
+  [selectCategoryReducer], //output of this are categories, if we had [selectCategoryReducer, selectCurrentUsers] then in line bellow we would have (categoriesSlice, currentUserSlice) => ...
+  (categoriesSlice) => categoriesSlice.categories //the only time where this will run is when we get different categoriesSliceObject we get back from selectCategoryReducer is different
+);
+
+export const selectCategoriesMap = createSelector(
+  [selectCategories],
+  (categories) => {
+    return categories.reduce((acc, category) => {
+      const { title, items } = category;
+      acc[title.toLowerCase()] = items;
+      return acc;
+    }, {});
+  }
+);
