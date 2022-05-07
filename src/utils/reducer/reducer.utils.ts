db7280df25970508d0c8eb7702c73580738a66d6
Slobodan Stojkovic <sc.slobodanstojkovic@gmail.com>
2022-05-07 21:56:40 +0200

    Migrate Category Reducer to TypeScript

---
+++ src/utils/reducer/reducer.utils.ts
@@ -0,0 +1,52 @@
+import { AnyAction } from "redux"; //an interface that extends upon Action type
+
+type Matchable<AC extends () => AnyAction> = AC & {
+  type: ReturnType<AC>["type"];
+  match(action: AnyAction): action is ReturnType<AC>;
+};
+
+export function withMatcher<AC extends () => AnyAction & { type: string }>(
+  actionCreator: AC
+): Matchable<AC>;
+
+export function withMatcher<
+  AC extends (...args: any[]) => AnyAction & { type: string }
+>(actionCreator: AC): Matchable<AC>;
+
+export function withMatcher(actionCreator: Function) {
+  const type = actionCreator().type;
+
+  return Object.assign(actionCreator, {
+    type,
+    match(action: AnyAction) {
+      return action.type === type;
+    },
+  });
+}
+
+export type ActionWithPayload<T, P> = {
+  type: T;
+  payload: P;
+};
+
+export type Action<T> = {
+  type: T;
+};
+
+//function overloading - provides us ability to make multiple function type definitions of the same name to have multiple type definitions for create action, and it allows this function to recieve different parametar types, depending on parametar types it recieves
+export function createAction<T extends string, P>(
+  type: T,
+  payload: P
+): ActionWithPayload<T, P>;
+
+export function createAction<T extends string>(
+  type: T,
+  payload: void
+): Action<T>;
+
+export function createAction<T extends string, P>(type: T, payload: P) {
+  return {
+    type,
+    payload,
+  };
+}
